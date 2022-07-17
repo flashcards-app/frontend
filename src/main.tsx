563ac8222a38961fb0e1a44cc3c373f6bbb2fcd4
Refaelbenzvi24 <refaelbenzvi24@gmail.com>
2022-07-18 00:16:38 +0300

    fix(main.tsx): wraped the app in strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react'
+import { Suspense, StrictMode } from 'react'
 import { BrowserRouter as Router, } from 'react-router-dom'
 // @ts-expect-error react types not compatible yet.
 import { createRoot } from 'react-dom/client'
@@ -11,18 +11,18 @@ import ReactQuery from './components/ReactQuery/reactQuery'
 import ProgressSpinner from './components/UI/Progress/ProgressSpinner'
 
 
-
-
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const root = createRoot(document.querySelector('#root') as Element)
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 root.render(
-	<ReactQuery>
-		<Router>
-			<Suspense fallback={<ProgressSpinner/>}>
-				<App/>
-			</Suspense>
-		</Router>
-	</ReactQuery>,
+	<StrictMode>
+		<ReactQuery>
+			<Router>
+				<Suspense fallback={<ProgressSpinner/>}>
+					<App/>
+				</Suspense>
+			</Router>
+		</ReactQuery>
+	</StrictMode>,
 )
