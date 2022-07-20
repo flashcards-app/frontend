99e4adb60c88c9785ca604e6e4924bc4750109fd
NoyaItzhaki <noinoya@gmail.com>
2022-07-20 16:58:50 +0300

    feat(questions page): created select menu for subject
    - added select cmp
    - gets options by simple array for now.

--- src/components/UI/Form/Select.tsx
+++ src/components/UI/Form/Select.tsx
@@ -1,14 +1,38 @@
-function SelectWithLabel() {
+import { DetailedHTMLProps, InputHTMLAttributes, TextareaHTMLAttributes } from "react";
+import { Row, Col } from "../Grid";
+
+interface SelectProps extends DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement> {
+	label: string,
+	options: string[],
+	onSelect: (arg0: string) => void,
+	error?: string
+}
+
+const style = "block w-52 py-2 px-3 border border-gray-300 bg-whiterounded-md shadow-sm focus:outline-none focus:ring-transparent focus:border-gray-400"
+
+function SelectWithLabel(props: SelectProps) {
+	const { label, options, onSelect, error } = props
 	return (
-		<label className="text-gray-700" htmlFor="animals">
-			Animals
-			<select
-				id="animals"
-				className="block w-52 py-2 px-3 border border-gray-300 bg-white
-		          rounded-md shadow-sm focus:outline-none focus:ring-transparent focus:border-gray-400">
-				<option value="">Select an option</option>
-			</select>
-		</label>
+		<>
+			<label className="text-gray-700" htmlFor="select-menu">
+				{label}
+				<select defaultValue="default"
+					onChange={(ev) => onSelect(ev.target.value)}
+					id="select-menu"
+					className={style}>
+					<option value="default" disabled hidden>Select one</option>
+					{options.length > 0 && options.map((option) => {
+						return <option key={option} value={option}>{option}</option>
+					})}
+				</select>
+			</label>
+			
+			<Row className="w-full min-h-[20px]">
+				<Col className="w-full">
+					<p className="px-2 pt-1 text-sm text-[#ff6767] dark:text-[#ff5050]">{error}</p>
+				</Col>
+			</Row>
+		</>
 	)
 }
 
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,6 +1,7 @@
 import { useFormik } from "formik"
 import * as Yup from "yup"
 import TextArea from "../../components/UI/Form/TextArea"
+import Select from "../../components/UI/Form/Select"
 import { questionsEndpoint } from "../../services"
 import Button from "../../components/UI/Buttons/Button"
 import Question from "../../modules/Entities/Question"
@@ -8,29 +9,39 @@ import Question from "../../modules/Entities/Question"
 
 export default () => {
 	const formik = useFormik({
-		initialValues:    {
+		initialValues: {
+			subject: '',
 			question: '',
-			answer:   ''
+			answer: '',
 		},
 		validationSchema: Yup.object({
+			subject: Yup.string().required('יש להזין נושא'),
 			question: Yup.string().required('יש להזין שאלה'),
-			answer:   Yup.string().required('יש להזין תשובה')
+			answer: Yup.string().required('יש להזין תשובה'),
 		}),
 		validateOnChange: false,
-		validateOnBlur:   false,
-		onSubmit:         async (values) => {
-			if (formik.isValid) {
-				const { question, answer } = values
-				const questionObject       = new Question({ question, answer })
-				await questionsEndpoint.create(questionObject)
-				formik.handleReset
-			}
+		validateOnBlur: false,
+		onSubmit: async (values) => {
+			const { question, answer, subject } = values
+			const questionObject = new Question({ question, answer, subject })
+			await questionsEndpoint.create(questionObject)
+			formik.handleReset()
 		}
 	})
+
 
 	return (
 		<div className="h-full w-full mx-auto px-50 pt-40">
 			<form onSubmit={formik.handleSubmit}>
+				<section>
+					<Select
+						label={'נושא'}
+						options={['React.js', 'Javascript', 'Angular']}
+						onSelect={(ev)=>formik.values.subject = ev}
+						id="subject"
+						onBlur={() => formik.validateField('subject')}
+						error={formik.errors.subject} />
+				</section>
 				<section>
 					<label>
 						שאלה
@@ -41,7 +52,7 @@ export default () => {
 						value={formik.values.question}
 						onChange={formik.handleChange}
 						onBlur={() => formik.validateField('question')}
-						error={formik.errors.question}/>
+						error={formik.errors.question} />
 				</section>
 
 				<section>
@@ -54,7 +65,12 @@ export default () => {
 						value={formik.values.answer}
 						onChange={formik.handleChange}
 						onBlur={() => formik.validateField('answer')}
+<<<<<<< HEAD
 						error={formik.errors.answer}/>
+=======
+						error={formik.errors.answer} />
+
+>>>>>>> a1ee624 (feat(questions page): created select menu for subject)
 				</section>
 
 				<div className="flex justify-center">
